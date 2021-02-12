4eaaab3bb5414bf31907bd4aa7f706a4a805864c
Leandro Segovia <ldlsegovia@gmail.com>
2021-02-12 13:06:56 -0300

    feat(tak): add method to get random users

--- app/models/lakatan/task.rb
+++ app/models/lakatan/task.rb
@@ -1,5 +1,18 @@
 module Lakatan
   class Task < ApplicationRecord
     include Lakatan::ApiResource
+
+    def random_user
+      raffle = api_resource_class.new(id: id).raffle
+      user_id = raffle.user_ids.first
+
+      if !user_id
+        raise Lakatan::Error.new(
+          "Can't find random user_id for Lakatan::Task ##{id}"
+        )
+      end
+
+      Lakatan::User.find_from_cache_or_create_from_api!(user_id)
+    end
   end
 end
--- spec/dummy/spec/models/lakatan/task_spec.rb
+++ spec/dummy/spec/models/lakatan/task_spec.rb
@@ -14,5 +14,54 @@ module Lakatan
         }
       end
     end
+
+    describe "#random_user" do
+      let(:user_id) { double }
+      let(:user_ids) { [user_id] }
+
+      let(:raffle) do
+        instance_double(
+          "Lakatan::Api::Raffle",
+          user_ids: user_ids
+        )
+      end
+
+      let(:task_api) do
+        instance_double(
+          "Lakatan::Api::Task",
+          raffle: raffle
+        )
+      end
+
+      let(:task) { create(:task) }
+      let(:user) { double }
+
+      before do
+        allow(Lakatan::Api::Task).to receive(:new).and_return(task_api)
+        allow(Lakatan::User).to receive(:find_from_cache_or_create_from_api!)
+          .and_return(user)
+      end
+
+      def perform
+        task.random_user
+      end
+
+      it "returns expected user" do
+        expect(perform).to eq(user)
+        expect(Lakatan::Api::Task).to have_received(:new).with(id: task.id)
+        expect(Lakatan::User).to have_received(:find_from_cache_or_create_from_api!)
+          .with(user_id)
+      end
+
+      context "with not found users" do
+        let(:user_ids) { [] }
+
+        it "returns expected user" do
+          expect { perform }.to raise_error(Lakatan::Error)
+          expect(Lakatan::Api::Task).to have_received(:new).with(id: task.id)
+          expect(Lakatan::User).not_to have_received(:find_from_cache_or_create_from_api!)
+        end
+      end
+    end
   end
 end
